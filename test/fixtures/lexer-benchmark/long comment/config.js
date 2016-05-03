a89b03d6b2648638dc3415e590a91525dfb615de
fatfisz <fatfisz@gmail.com>
2016-05-03 19:24:18 +0200

    Add lexer benchmark.

---
+++ test/fixtures/lexer-benchmark/long comment/config.js
@@ -0,0 +1,17 @@
+'use strict';
+
+module.exports = {
+  timeout: 5000,
+  type: 'success',
+  result: {
+    start: {
+      line: 1,
+      column: 1,
+    },
+    end: {
+      line: 100003,
+      column: 1,
+    },
+    tokens: [],
+  },
+};
